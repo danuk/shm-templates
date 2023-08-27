0477c10551844da5ce78e55b289d1df55dedf9f2
Daniil Firsov <daniil.firsov@pimpay.ru>
2023-08-27 15:06:20 +0300

    dnk: turn off colors

--- marzban/marzban.sh
+++ marzban/marzban.sh
@@ -14,23 +14,7 @@ colorized_echo() {
     local color=$1
     local text=$2
 
-    case $color in
-        "red")
-        printf "\e[91m${text}\e[0m\n";;
-        "green")
-        printf "\e[92m${text}\e[0m\n";;
-        "yellow")
-        printf "\e[93m${text}\e[0m\n";;
-        "blue")
-        printf "\e[94m${text}\e[0m\n";;
-        "magenta")
-        printf "\e[95m${text}\e[0m\n";;
-        "cyan")
-        printf "\e[96m${text}\e[0m\n";;
-        *)
-            echo "${text}"
-        ;;
-    esac
+    echo "${text}"
 }
 
 check_running_as_root() {
